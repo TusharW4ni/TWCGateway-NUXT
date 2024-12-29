ab5ca193828d167b65b8afd25a54e33350434c70
TusharW4ni <reachtusharwani@gmail.com>
2024-12-29 17:56:44 -0600

    pagination fixed styling

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -34,33 +34,28 @@ function prevPage() {
 
 <template>
   <div
-    class="flex justify-center items-center bg-white p-2 rounded-lg border-2 space-x-20"
+    class="flex justify-center items-center bg-white p-2 rounded-lg border-2 space-x-4"
   >
     <button
-      class="flex justify-center items-center bg-gray-100 w-1/2 py-2 border-2 border-gray-200 rounded text-gray-200 hover:border-gray-900 hover:text-black disabled:cursor-not-allowed disabled:border-gray-100 disabled:bg-gray-100 disabled:text-gray-200"
+      class="flex justify-center items-center bg-gray-100 p-2 border-2 border-gray-200 rounded text-gray-200 hover:border-gray-900 hover:text-black disabled:cursor-not-allowed disabled:border-gray-100 disabled:bg-gray-100 disabled:text-gray-200"
       @click="prevPage"
       :disabled="disablePrevPage"
     >
       <ChevronLeftIcon class="w-5" />
     </button>
 
-    <div>
-      <span
-        v-if="!loading"
-        class="flex justify-center items-center bg-gray-100 p-2 rounded"
-      >
+    <!-- Added a fixed width to prevent content shifting -->
+    <div class="flex justify-center items-center bg-gray-100 p-2 rounded w-20">
+      <span v-if="!loading">
         {{ props.currentPage }}/{{ props.totalPages }}
       </span>
-      <span
-        v-else
-        class="flex justify-center items-center bg-gray-100 p-2 rounded"
-      >
-        <img src="/assets/blocks-shuffle-3.svg" class="w-10" />
+      <span v-else>
+        <img src="/assets/blocks-shuffle-3.svg" class="h-6 w-5" />
       </span>
     </div>
 
     <button
-      class="flex justify-center items-center bg-gray-100 w-1/2 py-2 border-2 border-gray-200 rounded text-gray-200 hover:border-gray-900 hover:text-black disabled:cursor-not-allowed disabled:border-gray-100 disabled:bg-gray-100 disabled:text-gray-200"
+      class="flex justify-center items-center bg-gray-100 p-2 border-2 border-gray-200 rounded text-gray-200 hover:border-gray-900 hover:text-black disabled:cursor-not-allowed disabled:border-gray-100 disabled:bg-gray-100 disabled:text-gray-200"
       @click="nextPage"
       :disabled="disableNextPage"
     >
