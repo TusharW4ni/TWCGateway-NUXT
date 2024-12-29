8b4a54a450a548f543310b62181084592b3d2305
TusharW4ni <reachtusharwani@gmail.com>
2024-12-29 15:22:11 -0600

    home route fixed

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,13 @@
-<script setup>
+<script setup lang="ts">
 async function logout() {
+  loggingOut.value = true;
   window.location.href = "/api/auth0/logout";
 }
+
+const loggingOut = ref(false);
 </script>
 
-<template>
+<!-- <template>
   <div class="flex flex-col h-screen justify-center items-center bg-black">
     <div class="text-white text-4xl">Hello, Nuxt!</div>
     <button
@@ -13,5 +16,26 @@ async function logout() {
     >
       Logout
     </button>
+  </div>
+</template> -->
+
+<template>
+  <div class="flex h-screen justify-center items-center">
+    <div class="flex flex-col items-center">
+      <div class="text-4xl">404</div>
+      <div class="mt-3 uppercase font-bold">page not found</div>
+      <div class="flex space-x-10 mt-3">
+        <button
+          v-if="!loggingOut"
+          class="border-2 px-2 py-1 rounded hover:bg-red-300 hover:text-black"
+          @click="logout"
+        >
+          Logout
+        </button>
+        <button v-if="loggingOut" class="border-2 px-2 py-1 rounded bg-white">
+          <img src="/assets/blocks-shuffle-3.svg" />
+        </button>
+      </div>
+    </div>
   </div>
 </template>
--- server/middleware/01.authentication.ts
+++ server/middleware/01.authentication.ts
@@ -6,18 +6,23 @@ import {
 } from "../api/auth0/redirect-urls";
 
 export default defineEventHandler(async (event) => {
+  console.log("this is being hit");
   const token = getCookie(event, "token") || "";
 
   if (!token && !event.node.req.url?.includes("/api/login-callback")) {
+    console.log("user is not logged in");
     sendRedirect(event, loginRedirectUrl());
   } else {
+    console.log("user is logged in");
     try {
       try {
         const claims = jwt.verify(
           token,
           fs.readFileSync(process.cwd() + "/cert-dev.pem")
         );
+        console.log("Claims from the JWT Token: \n", claims);
         event.context.claims = claims;
+        console.log("event.context", event.context);
       } catch (e) {
         console.error(e);
         sendRedirect(event, logoutRedirectUrl(token));
